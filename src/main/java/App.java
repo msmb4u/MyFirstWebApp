a9e29eb51a98012d03ab305edcc4ed5b65f7beee
Subbu <msmb4u@gmail.com>
2018-07-29 00:40:00 +0530

    changed app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,23 @@
 
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println("<<<<<<In <<<<<< main<");
+	public App() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public static void main(String[] args) {
+		System.out.println("This is my first maven application");
+		display(10,20);
+	}
+	
+	static void display(int x,int y){
+		int c;
+		c =x+y;
+		System.out.println("Count of "+c);
+		int i =0;
+		while(x>i){
+			
+		}
 	}
 
 }
